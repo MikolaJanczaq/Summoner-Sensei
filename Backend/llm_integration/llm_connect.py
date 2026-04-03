7eaa44c7493a37f0949d8dde02e929435d602dd4
Mikołaj Janczak <mikolaj.janczaq@gmail.com>
2026-04-03 13:47:51 +0200

    Extract prompt creation to seperate function

--- Backend/llm_integration/llm_connect.py
+++ Backend/llm_integration/llm_connect.py
@@ -2,12 +2,7 @@ from listener.models import GameState, Event
 from llm_integration.lm_studio_integration import call_lm_studio
 
 
-async def ask_llm(
-        game_state: GameState,
-        new_events: list[Event]
-) -> str:
-    """Translates python objects into LLM events and connecting to API"""
-
+def promp_creator(game_state: GameState, new_events: list[Event]) -> str:
     context_prompt = f"Game time: {game_state.game_time // 60} minutes"
     context_prompt += (f"You play as a {game_state.me.champion} "
                        f"(KDA: {game_state.me.scores.kills}/{game_state.me.scores.deaths}/{game_state.me.scores.assists}."
@@ -23,14 +18,25 @@ async def ask_llm(
         "\nBased on this information, give me ONE strictly tactical and actionable advice \n"\
         "Be very concise, maximum 2 sentences."
 
+    return context_prompt
+
+
+async def ask_llm(
+        game_state: GameState,
+        new_events: list[Event]
+) -> str:
+    """Translates python objects into LLM events and connecting to API"""
+
+    created_prompt = promp_creator(game_state, new_events)
+
     # print("----SENDING PROMPT TO LLM-----")
-    # print(context_prompt)
+    # print(created_prompt)
     # print("------------------------------")
 
-    response = await call_lm_studio(context_prompt)
+    response = await call_lm_studio(created_prompt)
 
     # print("----RECEIVED ADVICE-----")
-    # print(response)
+    # print(created_prompt)
     # print("------------------------------")
 
     return response
